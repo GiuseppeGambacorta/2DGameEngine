b1145e658f0077db23a141d3b908658ec19bb76d
Giuseppe Gambacorta <gambacortagiuseppe@outlook.it>
2023-12-24 18:01:10 +0100

    engine writed

--- src/App.java
+++ src/App.java
@@ -1,5 +1,11 @@
+import core.GameEngine;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+
+	public static void main(String[] args) {
+		GameEngine engine = new GameEngine();
+		engine.mainLoop();
+	}
+
 }
+
---
+++ src/core/GameEngine.java
@@ -0,0 +1,53 @@
+package core;
+
+import java.util.logging.*;
+
+
+public class GameEngine  {
+
+	final private long period = 20; /* 20 ms = 50 frame al secondo */
+	final private Logger logger = Logger.getLogger("GameEngine");
+	
+	public GameEngine(){
+	}
+	
+	public void mainLoop(){
+		long lastTime = System.currentTimeMillis();
+		while(true){
+			long current = System.currentTimeMillis();
+			int elapsed = (int)(current - lastTime);
+			processInput();
+			updateGame(elapsed);
+			render();
+			waitForNextFrame(current);
+			lastTime = current;
+		}
+	}
+
+	/**
+	 * Take a little nap to synch with the frame rate
+	 * 
+	 * @param current
+	 */
+	protected void waitForNextFrame(long current){
+		long dt = System.currentTimeMillis() - current;
+		if (dt < period){
+			try {
+				Thread.sleep(period-dt);
+			} catch (Exception ex){}
+		}
+	}
+	
+	protected void processInput(){
+		logger.log(Level.INFO, "..process input..");
+	}
+	
+	protected void updateGame(int elapsed){
+		logger.log(Level.INFO, "..update game: elapsed "+elapsed);
+	}
+	
+	protected void render(){
+		logger.log(Level.INFO, "..render..");
+	}
+
+}
