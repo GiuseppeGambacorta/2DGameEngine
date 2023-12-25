a3418008c52e6a95af36721d73537be7e1949767
Giuseppe Gambacorta <gambacortagiuseppe@outlook.it>
2023-12-25 20:45:07 +0100

    added a map for scene and panel

--- src/core/GameEngine.java
+++ src/core/GameEngine.java
@@ -1,7 +1,9 @@
 package core;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.logging.*;
 
 
@@ -18,20 +20,21 @@ public class GameEngine  {
 	final private long period = 20; /* 20 ms = 50 frame al secondo */
 	final private Logger logger = Logger.getLogger("GameEngine");
 	
-	final private List<Scene> scenes;
-	final private List<ScenePanel> panels;
+	final private Map<Scene, ScenePanel> scenePanelMap;
 	private Scene currentScene;
 	private ScenePanel currentPanel;
 	final private View window;
 
 	public GameEngine(){
-		scenes = new ArrayList<>();
-		scenes.add(new GameScene());
-		currentScene = scenes.get(0);
+		scenePanelMap = new HashMap<>();
 
-		panels = new ArrayList<>();
-		panels.add(new GameScenePanel(currentScene, 600, 600));
-		currentPanel = panels.get(0);
+        Scene gameScene = new GameScene();
+        ScenePanel gameScenePanel = new GameScenePanel(gameScene, 600, 600);
+
+        scenePanelMap.put(gameScene, gameScenePanel);
+
+        currentScene = gameScene;
+        currentPanel = gameScenePanel;
 		
 		window = new wievimpl(currentPanel, "engine", 600, 600);
 	}
